ec43d45fb826e21ff2b3a31664bc4cd95bc36d82
tareimarcello <116428202+tareimarcello@users.noreply.github.com>
2024-10-02 18:07:53 +0200

    INTRODOTTE alcune dao

--- src/main/java/logic/appcontroller/SignupController.java
+++ src/main/java/logic/appcontroller/SignupController.java
@@ -26,5 +26,4 @@ public class SignupController {
 
 
     }
-    //ciao a ttui e tutte
 }
--- src/test/TestDB.java
+++ src/test/TestDB.java
@@ -1,18 +1,11 @@
 import logic.appcontroller.SignupController;
 import logic.beans.Customerbean;
-import logic.connection.ConfigFileReader;
 import org.junit.jupiter.api.Test;
 
-public class TestDB {
+class TestDB {
 
     @Test
-    public void testConfigFile(){
-
-        ConfigFileReader conf = new ConfigFileReader();
-
-    }
-    @Test
-    public void testCustomerInsert(){
+    void testCustomerInsert(){
 
         Customerbean customer = new Customerbean ("Marcello","Tarei","mtarei@gmail.com","1234");
         SignupController controller = new SignupController();

d3dfa80d423ffa726b9f7a9fed7c49dd0967e48d
tareimarcello <116428202+tareimarcello@users.noreply.github.com>
2024-11-25 14:50:27 +0100

    final git

--- src/main/java/logic/exceptions/Exceptions.java
+++ src/main/java/logic/exceptions/Exceptions.java
@@ -31,49 +31,5 @@ public final class Exceptions {
             System.exit(0);
         }
     }
-    public static void exceptionSignupOccurred(ActionEvent event){
-        Alert alert = new Alert(Alert.AlertType.WARNING);
-        alert.setTitle("Signup failed");
-        alert.setHeaderText("We weren't be able to retrieve your data, please try  again!");
-        alert.setContentText("Please, fill Email and Password Fields. They cannot be empty.");
-        Optional<ButtonType> result = alert.showAndWait();
-        if(result.isPresent() && result.get() == ButtonType.OK){
-            event.consume();
-        }
-        else{
-            Page controller=new Page();
-            controller.backTo();
-        }
-    }
-    public static String exceptionInputChatOccurred(String text) {
-        Alert alert = new Alert(Alert.AlertType.WARNING);
-        alert.setTitle("Sending message failed");
-        alert.setHeaderText("We weren't be able to retrieve your data, please try  again!");
-        alert.setContentText("Your text can't contain any char ");
-        Optional<ButtonType> result = alert.showAndWait();
-        if(result.isPresent() && result.get() == ButtonType.OK){
-            return text;
-        }
-        else{
-            return "";
-        }
-    }
-    public static void exceptionEditProfileOccurred(ActionEvent event) {
-        Alert alert = new Alert(Alert.AlertType.WARNING);
-        alert.setTitle("Update profile failed");
-        alert.setHeaderText("We weren't be able edit your data, please try  again!");
-        alert.setContentText("Please,checkActive if " +
-                "tag and description begin in '#'," +
-                "the data is in format 'day-month-year'" +
-                "text input not contain char like / ' \" ");
-        Optional<ButtonType> result = alert.showAndWait();
-        if(result.isPresent() && result.get() == ButtonType.OK){
-            event.consume();
-        }
-        else {
-            PageMenu controller=new PageMenu();
-            controller.backTo();
-        }
-    }
 }
 

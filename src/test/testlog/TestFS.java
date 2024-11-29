4549aa96300997b2e0655caa91ea44eda0ec6320
tareimarcello <116428202+tareimarcello@users.noreply.github.com>
2024-11-29 11:00:17 +0100

    final git

--- src/test/testlog/TestFS.java
+++ src/test/testlog/TestFS.java
@@ -6,6 +6,7 @@ import logic.model.Message;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -16,16 +17,35 @@ class TestFS {      //Tarei Marcello
         msgBean.setIdChat(12);
         msgBean.setIdSender(4);
         msgBean.setIdReceiver(7);
-        msgBean.setText("Ciao sono test file system");
+        msgBean.setText("Ciao sono test file system 5");
         DAOMessageImplFileSystem msgDao = new DAOMessageImplFileSystem();
         Message msg = new Message(-1, msgBean.getIdSender(), msgBean.getIdReceiver(), msgBean.getText(), new Timestamp(System.currentTimeMillis()), msgBean.getIdChat());
         msgDao.save(msg);
-        return true;
+        List<Message> messages = msgDao.getAll();
+        long lastId = 0;
+        for (int i=0;i<messages.size()-1;i++) {
+            if(messages.get(i).getIdMsg() > messages.get(i+1).getIdMsg()){
+                lastId = messages.get(i).getIdMsg();
+            }else{
+                lastId = messages.get(i+1).getIdMsg();
+            }
+        }
+        if(msgDao.get(lastId).getSender()==msgBean.getIdSender() && msgDao.get(lastId).getReceiver()==msgBean.getIdReceiver() && msgDao.get(lastId).getText().equals(msgBean.getText()) && msgDao.get(lastId).getIdChat()==msgBean.getIdChat()){
+            return true;
+        }else{
+            return false;
+        }
     }
 
     public boolean messageDaoFsGet(){
         DAOMessageImplFileSystem msgDao = new DAOMessageImplFileSystem();
-        return msgDao.get(20) != null;
+        if (msgDao.get(20) != null){
+            Message msg= msgDao.get(20);
+            if(msg.getSender()==4 && msg.getReceiver()==7 && msg.getText().equals("ciao") && msg.getIdChat()==4 && msg.getTime().equals(Timestamp.valueOf("2024-11-18 19:17:37.923"))){
+                return true;
+            }
+        }
+        return false;
     }
 
     public boolean messageDaoFsUpdate(){
@@ -34,14 +54,18 @@ class TestFS {      //Tarei Marcello
         msgBean.setIdChat(6);
         msgBean.setIdSender(4);
         msgBean.setIdReceiver(44);
-        msgBean.setText("Ciao sono test update file system");
-        Message msg = new Message(29, msgBean.getIdSender(), msgBean.getIdReceiver(), msgBean.getText(), new Timestamp(System.currentTimeMillis()), msgBean.getIdChat());
+        msgBean.setText("Ciao sono test update file system 3");
+        Message msg = new Message(30, msgBean.getIdSender(), msgBean.getIdReceiver(), msgBean.getText(), new Timestamp(System.currentTimeMillis()), msgBean.getIdChat());
         try {
             msgDao.update(msg);
+            if(msgDao.get(30).getText().equals(msgBean.getText())){
+                return true;
+            }else{
+                return false;
+            }
         }catch(IllegalArgumentException e){
             return false;
         }
-        return true;
     }
 
     public boolean messageDaoFsDelete(){

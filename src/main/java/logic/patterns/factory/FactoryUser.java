11820d44b5347a437fb0bd741f0fa4be7451d36e
tareimarcello <116428202+tareimarcello@users.noreply.github.com>
2025-02-12 15:54:08 +0100

    final git

--- src/main/java/logic/patterns/factory/FactoryUser.java
+++ src/main/java/logic/patterns/factory/FactoryUser.java
@@ -2,19 +2,18 @@ package logic.patterns.factory;
 
 import logic.model.Chef;
 import logic.model.Customer;
-import logic.model.User;
+
 
 public  class FactoryUser {
 
-    public Chef createChef(long id,String name, String surname, String email, String password, String restaurant,String bestDish){
-        return new Chef(id, name, surname, email, password, restaurant, bestDish);
+    public Chef createChef(long id,String name, String surname, String email, String password, String restaurant,String bestDish,String city){
+        Chef ch =  new Chef(id, name, surname, email, password, restaurant, bestDish);
+        ch.setCitta(city);
+        return ch;
     }
 
     public Customer createCustomer(long id,String name, String surname, String email, String password,boolean bookNot){
         return new Customer(id, name, surname, email, password, bookNot);
     }
 
-    public User create(long id, String name, String surname, String email, String password, String restaurant, String bestDish){
-        return null;
-    }
 }
